11b2dcc0801e8fe453c96183339f68738d2867ad
yukaina <misaki.yukaina@gmail.com>
2016-01-02 16:59:30 +0900

    add CodeClimate::TestReporter.start

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'pddn'
 require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
     SimpleCov::Formatter::HTMLFormatter,
     CodeClimate::TestReporter::Formatter
