f27e9d19d493482239a6d78f4e926fe63d37a166
yukaina <misaki.yukaina@gmail.com>
2016-07-25 02:33:57 +0900

    2016_renewals
    edit for rubocup.

--- lib/pddn/pages/ip_damho_map.rb
+++ lib/pddn/pages/ip_damho_map.rb
@@ -1,7 +1,7 @@
 module Pddn
-  # for 国土交通省【川の防災情報】ダム放流通知全国地図
-  # path /kawabou/ipDamhoMap.do?areaCd=81&gamenId=01-0601&fldCtlParty=no
   module Pages
+    # for 国土交通省【川の防災情報】ダム放流通知全国地図
+    # path /kawabou/ipDamhoMap.do?areaCd=81&gamenId=01-0601&fldCtlParty=no
     class IpDamhoMap
       attr_reader :doc, :discharge_history_uri
       def initialize(page_doc)
@@ -16,11 +16,10 @@ module Pddn
       private
 
       def init_discharge_uri(doc)
-        @somewhere_discharge = doc.xpath('id("ykh-menu")/div/div[position()>2]/table/tbody/tr').length > 0
+        @somewhere_discharge = !doc.xpath('id("ykh-menu")/div/div[position()>2]/table/tbody/tr').empty?
         discharge_history_a_link = doc.xpath('id("ykh-control2")/table/tr/td/a').first
         @discharge_history_uri = discharge_history_a_link && discharge_history_a_link[:href]
       end
     end
   end
 end
-
