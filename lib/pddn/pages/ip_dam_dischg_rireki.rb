8a2e64b061b7edb9593d153b35a206f2dd592fdc
yukaina <misaki.yukaina@gmail.com>
2016-07-25 02:30:58 +0900

    2016_renewals
    edit for rubocup.

--- lib/pddn/pages/ip_dam_dischg_rireki.rb
+++ lib/pddn/pages/ip_dam_dischg_rireki.rb
@@ -1,5 +1,6 @@
 module Pddn
   module Pages
+    # 地域ごとのダム放流通知発表状況のページ
     class IpDamDischgRireki
       attr_reader :doc, :discharge_announcements
 
@@ -7,18 +8,24 @@ module Pddn
         @doc = page_doc
         @discharge_announcements = []
 
+        build_discharge_announcements(doc)
+        build_discharge_notices(doc)
+      end
+
+      private
+
+      def build_discharge_announcements(doc)
         doc.xpath('//div[5]/table[1]/tr').each do |tr|
           announcement = Pddn::Announcement.new(tr)
           @discharge_announcements << announcement unless announcement.nil?
         end
+      end
 
+      def build_discharge_notices(doc)
         doc.xpath('//div[7]/table/tr').zip(@discharge_announcements).each do |tr, announcement|
           tr.xpath('td').each do |td|
             notice = Pddn::Notice.new(td)
-            unless notice.dam_dischg_code.nil?
-              announcement.discharge_notices << notice
-              # announcement.dam_dischg_code = notice.dam_dischg_code
-            end
+            announcement.discharge_notices << notice unless notice.dam_dischg_code.nil?
           end
         end
       end
