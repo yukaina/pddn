ea8c9d1bdbe0e3c3ebdd2ebbe5e0be3614246213
yukaina <misaki.yukaina@gmail.com>
2016-07-25 23:17:13 +0900

    add Test Coverage setting.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,10 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'pddn'
+require 'codeclimate-test-reporter'
+
+SimpleCov.start do
+  formatter SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    CodeClimate::TestReporter::Formatter
+  ]
+end
