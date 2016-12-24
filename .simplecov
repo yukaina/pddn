916e6ab6b608acae332a8eb5d7d4182d8d70cccb
yukaina <misaki.yukaina@gmail.com>
2016-12-25 02:09:29 +0900

    edit .simplecov, circle.yml and top_spec for rubocop.

--- .simplecov
+++ .simplecov
@@ -1,9 +1,4 @@
-require 'codeclimate-test-reporter'
 SimpleCov.start do
   add_filter '/bin/'
   add_filter '/spec/'
-  formatter SimpleCov::Formatter::MultiFormatter[
-              SimpleCov::Formatter::HTMLFormatter,
-              CodeClimate::TestReporter::Formatter
-            ]
 end
--- spec/pddn/top_spec.rb
+++ spec/pddn/top_spec.rb
@@ -5,10 +5,19 @@ describe 'Top' do
     Nokogiri::HTML(File.open('spec/html/ipYokeihoZenkokuMap.html', 'r+:utf-8', &:read))
   end
 
-  context do
-    subject { Pddn::Top.new(top_page_doc) }
-    it { expect(subject.doc).to eq(top_page_doc) }
-    it { expect(subject.somewhere_discharge?).to be_truthy }
-    it { expect(subject.discharge_uris.first.to_s).to eq 'ipDamDischgRireki.do?areaCd=81&gamenId=01-0602&fldCtlParty=no' }
+  describe '#doc' do
+    subject { Pddn::Top.new(top_page_doc).doc }
+    it { expect(subject).to eq(top_page_doc) }
+  end
+
+  describe '#somewhere_discharge?' do
+    subject { Pddn::Top.new(top_page_doc).somewhere_discharge? }
+    it { expect(subject).to eq true }
+  end
+
+  describe '#discharge_uris' do
+    subject { Pddn::Top.new(top_page_doc).discharge_uris }
+    it { expect(subject.first).to eq 'ipDamDischgRireki.do?areaCd=81&gamenId=01-0602&fldCtlParty=no' }
+    it { expect(subject.last).to eq 'ipDamDischgRireki.do?areaCd=90&gamenId=01-0602&fldCtlParty=no' }
   end
 end
